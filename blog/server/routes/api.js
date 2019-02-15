81db0c5320d25e4cf9658e9b6f560914d1e56992
Ruchi <ruchikharwar4@gmail.com>
2019-02-15 18:28:40 +0530

    product-hunt

--- blog/server/routes/api.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const postsController = require('../controller/postsController')
-
-
-router.post('/api/article', postsController.createPosts)
